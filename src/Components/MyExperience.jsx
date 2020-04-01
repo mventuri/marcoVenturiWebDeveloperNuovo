cfb88991b6cffa81d7a79f0867706cbf069971d0
Mauro <re.mauro@live.com>
2020-04-01 20:19:51 +0200

    fix

--- src/Components/MyExperience.jsx
+++ src/Components/MyExperience.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import experience from "../shared/experience";
 import CompaniesCard from "./ExperienceCard";
 
