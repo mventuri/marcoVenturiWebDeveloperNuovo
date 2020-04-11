8573a6c71f9bc4ca65a61c577b6141b451f55fc1
marco venturi <mventuri10@gmail.com>
2020-04-11 20:24:30 +0200

    linkedin fix footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteIcon from "@material-ui/icons/Favorite";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import '../styles/footer.css'
+import "../styles/footer.css";
 
-const linkedinUrl = 'https://www.linkedin.com/in/marcoventuri/"'
+const linkedinUrl = "https://www.linkedin.com/in/marcoventuri/";
 
 function Footer() {
   return (
@@ -13,19 +13,18 @@ function Footer() {
         <Grid item xs={12}>
           <span className="space">
             Made with <FavoriteIcon /> and React by Marco Venturi.
-            </span>
+          </span>
           <span>
             Let's keep in touch
-              <a href={linkedinUrl} target="_blank" rel="noopener noreferrer"> <LinkedInIcon /> </a>
+            <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+              {" "}
+              <LinkedInIcon />{" "}
+            </a>
           </span>
-
         </Grid>
-
-
       </div>
-
     </Grid>
   );
 }
 
-export default Footer
+export default Footer;
